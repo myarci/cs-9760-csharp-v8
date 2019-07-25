ecefa4717b0dc1dadaffc77308f696b9dc252e78
Metin Yarci <metin.yarci@aurea.com>
2019-07-25 13:09:18 +0300

    revision1

---
+++ directory3/directory4/AParentClass.cs
@@ -0,0 +1,25 @@
+namespace directory3.directory4
+{	
+	public class AParentClass
+	{
+	    public void DoWork()
+	    {
+	    }
+
+	    public void ReportWork()
+	    {
+	    }
+
+		public class AnInnerClass
+		{
+		    public void DoWork()
+		    {
+		    }
+
+		    public void ReportWork()
+		    {
+		    }
+		}
+
+	}
+}
